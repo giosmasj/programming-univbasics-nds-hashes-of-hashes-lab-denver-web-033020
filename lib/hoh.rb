d481efec0badce72745a9969c7b40ac669319bad
James Giosmas <giosmasj@gmail.com>
2020-03-14 02:45:17 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,28 +31,22 @@
 # }
 
 def naming_system;
-  {label: "Kingdom",
+  {{label: "Kingdom"},
    sub_category: {
-     label: "Phylum",
+     label: "Phylum"},
      sub_category: {
-       label: "Class"
+       label: "Class"}
 {
-   label: "Order"
+   label: "Order"}
 {
-   label: "Family",
+   label: "Family"},
    sub_category: {
-     label: "Genus",
+     label: "Genus"},
 {
-   label: "Species",
+   label: "Species"},
    sub_category: nil
  }
      }
-   }
-   }
-   }
-  }
-  }
-  }
   # Remember:
   #  Kingdom
   #  Phylum
